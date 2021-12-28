0df9d7c8a917458931074a1f700230a5b01c5588
Abdumutalib Mirzajonov <Abdumutalib1989>
2021-12-28 18:02:12 +0500

    serializer tugatildi

--- CRM_shartnoma/Shartnoma_app/serializers.py
+++ CRM_shartnoma/Shartnoma_app/serializers.py
@@ -6,4 +6,17 @@ class KursSerializer(ModelSerializer):
         model = Kurs
         fields = ["nom", "narx"]
 
+class UstozSerializer(ModelSerializer):
+    class Meta:
+        model = Ustoz
+        fields = ["ism", "yosh", "tel"]
 
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ["ism", "familya", "sharif", "jins", "tel1", "tel2", "status"]
+
+class ShartnomaSerializer(ModelSerializer):
+    class Meta:
+        model = Shartnoma
+        fields = ["K", "shartnoma_raqami", "student", "kurs", "ustoz", "kunlari", "kurs_vaqti", "guruh_nomi", "tuzuvchi", "sinov_dars_sanasi", "status"]
