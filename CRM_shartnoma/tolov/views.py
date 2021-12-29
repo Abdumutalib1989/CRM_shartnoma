8e9a1696651bbee8d4418493ed489c11fd61350c
Abdumutalib Mirzajonov <Abdumutalib1989>
2021-12-29 10:34:13 +0500

    tolov view tugadi

--- CRM_shartnoma/tolov/views.py
+++ CRM_shartnoma/tolov/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from .models import Tolov
+from .serializers import TolovSerializer
 
-# Create your views here.
+
+class TolovListCreateAPIView(ListCreateAPIView):
+    queryset = Tolov.objects.all()
+    serializer_class = TolovSerializer
+
+class TolovGetUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Tolov.objects.all()
+    serializer_class = TolovSerializer
